8f68e5258ecd5992d713b874cb5dc8930919c638
Thne <451101751@qq.com>
2024-12-15 18:58:50 +0800

    pass 00-03
    pass 00-03

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -1,9 +1,10 @@
-// TODO: Fix the compiler error on this function.
 fn picky_eater(food: &str) -> &str {
     if food == "strawberry" {
         "Yummy!"
+    } else if food == "potato" {
+        "I guess I can eat that."
     } else {
-        1
+        "No thanks!"
     }
 }
 
@@ -11,15 +12,12 @@ fn main() {
     // You can optionally experiment here.
 }
 
-// TODO: Read the tests to understand the desired behavior.
-// Make all tests pass without changing them.
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn yummy_food() {
-        // This means that calling `picky_eater` with the argument "strawberry" should return "Yummy!".
         assert_eq!(picky_eater("strawberry"), "Yummy!");
     }
 
